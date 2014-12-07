6fdf51dd8e71a67f7fa607081f3de0b6b39325f2
Bruno Borges <bruno.borges@oracle.com>
2014-12-07 04:37:12 -0200

    cache-server runs as daemon

--- dockCacheServer.sh
+++ dockCacheServer.sh
@@ -11,7 +11,7 @@ then
 fi
 
 # RUN THE DOCKER COMMAND
-docker run -ti \
+docker run -d \
  --net=host \
  -v $1:/u01/oracle/coherence_config \
  $DOCKER_IMAGE_NAME java -cp /u01/oracle/coherence_config:/u01/oracle/coherence/coherence/lib/coherence.jar \
